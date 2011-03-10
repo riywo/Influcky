5760293f3f3e14d838e57db0bb1503b250643e83
riywo <riywo.jp@gmail.com>
2011-03-11 03:46:48 +0900

    initial commit

---
+++ lib/Infra.pm
@@ -0,0 +1,32 @@
+package Infra;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Infra -
+
+=head1 SYNOPSIS
+
+  use Infra;
+
+=head1 DESCRIPTION
+
+Infra is
+
+=head1 AUTHOR
+
+riywo E<lt>riywo.jp@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
